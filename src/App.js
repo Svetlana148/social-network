fd62bcfd70068637168488c433b446071323be91
Svetlana <sveta@gte.to>
2023-12-22 19:49:05 +0100

    94 React.lazy    React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import './App.css';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {connect, Provider} from 'react-redux';
+import store from './redux/redux-store.js';
 
-import {Routes, Route} from 'react-router-dom';
 
-// import Header from './components/Header/Header.jsx';
-import Navbar from './components/Navbar/Navbar.jsx';
-import News from './components/News/News.jsx';
-import Music from './components/Music/Music.jsx';
-import Settings from './components/Settings/Settings.jsx';
-
-import DialogsContainer from './components/Dialogs/DialogsContainer.jsx';
-import UsersContainer from './components/Users/UsersContainer.jsx';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import HeaderContainer from './components/Header/HeaderContainer';
-import Login from './components/Login/Login.jsx';
-// import {getAuthUserData} from './redux/auth-reducer.js';
-import {connect} from 'react-redux';
 import { initializeApp } from "./redux/app-reducer.js";
+import './App.css';
+import Navbar from './components/Navbar/Navbar.jsx';
+import HeaderContainer from './components/Header/HeaderContainer';
 import Preloader from './components/common/preloader/Preloader.jsx';
 
-import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux';
-import store from './redux/redux-store.js';
+
+
+// import News from './components/News/News.jsx';
+// import Music from './components/Music/Music.jsx';
+// import Settings from './components/Settings/Settings.jsx';
+// import DialogsContainer from './components/Dialogs/DialogsContainer.jsx';
+// import UsersContainer from './components/Users/UsersContainer.jsx';
+// import ProfileContainer from './components/Profile/ProfileContainer';
+// import Login from './components/Login/Login.jsx';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer.jsx'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer.jsx'));
+const Login  = React.lazy(() => import('./components/Login/Login.jsx'));
+const Music = React.lazy(() => import('./components/Music/Music.jsx'));
+const News = React.lazy(() => import('./components/News/News.jsx'));
+const Settings = React.lazy(() => import('./components/Settings/Settings.jsx'));
+
+
 
 
 class App extends Component {
@@ -45,7 +52,31 @@ class App extends Component {
 
 
           <div className='app-wrapper-content'> 
-            <Routes>  
+
+
+
+          <React.Suspense fallback={<div><Preloader /></div>}>
+						<Routes>
+							{/* <Route path="/profile/:userID" element={<ProfileContainer />} /> */}
+							<Route path='/Profile/:userId?' element={<ProfileContainer />} />
+							<Route path='/Dialogs/*' element={<DialogsContainer />} />
+              <Route path="/users" element={<UsersContainer />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/music" element={<Music />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/settings" element={<Settings />} />
+
+							{/* <Route path="/news" element={<News />} />
+							<Route path="/music" element={<Music />} />
+							<Route path="/users" element={<UsersContainer />} />
+							<Route path="/settings" element={<Settings />} />
+							<Route path="/login" element={<Login />} /> */}
+						</Routes>
+					</React.Suspense>
+
+
+
+            {/* <Routes>  
               <Route path='/Login/*' element={<Login />} />          
               <Route path='/Profile/:userId?' element={<ProfileContainer />} />
 
@@ -56,7 +87,7 @@ class App extends Component {
               <Route path='/News/*' element={<News />} />
               <Route path='/Music/*' element={<Music />} />
               <Route path='/Settings/*' element={<Settings />} />
-            </Routes>
+            </Routes> */}
           </div>
         </div>
     );
