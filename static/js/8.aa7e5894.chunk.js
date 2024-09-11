3b5f995acc5070c45ba0921b54d47ddb6c751138
Svetlana <sveta@gte.to>
2024-09-11 15:36:53 +0200

    Updates

--- static/js/8.aa7e5894.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreact_4_lesson=this.webpackJsonpreact_4_lesson||[]).push([[8],{273:function(e,s,n){},277:function(e,s,n){"use strict";n.r(s);var t=n(0),c=n.n(t);n(273);s.default=e=>c.a.createElement("div",null,"Music")}}]);
-//# sourceMappingURL=8.aa7e5894.chunk.js.map
--- static/js/9.e397534e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreact_4_lesson=this.webpackJsonpreact_4_lesson||[]).push([[9],{274:function(e,s,n){},278:function(e,s,n){"use strict";n.r(s);var t=n(0),a=n.n(t);n(274);s.default=e=>a.a.createElement("div",null,"News")}}]);
-//# sourceMappingURL=9.e397534e.chunk.js.map
